ba24cd7c6f59496213932dddcb105f2d3b48806a
Kunwardeep-26 <73779166+Kunwardeep-26@users.noreply.github.com>
2023-03-05 15:38:44 +0530

    Add files via upload

---
+++ src/components/nav.svelte
@@ -0,0 +1,8 @@
+<nav class="">
+    <ul class=" flex text-red-600 ">
+        <li class="">Have Notes</li>
+        <li class="text-right">Home</li>
+        <li class="text-right">Classes</li>
+        <li class="text-right">Upload</li>
+    </ul>
+</nav> 
