a4bfe4b047850600dda9475d7404876e0a06ed80
Kunwardeep-26 <73779166+Kunwardeep-26@users.noreply.github.com>
2023-03-06 20:27:26 +0530

    Add files via upload

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,8 +1,8 @@
-<nav class=" flex items-center justify-center bg-slate-800 h-14">
-    <ul class=" flex text-white justify-center">
-        <li class="float-right ml-3"><a href="/">Home</a></li>
-        <li class="float-right ml-3"><a href="/classes">Classes</a></li>
-        <li class="float-right ml-3 mr-36"><a href="/upload">Upload</a></li>
-        <span class="text-white ml-auto text-center">Have Notes</span>
+<nav class="bg-slate-800 h-14">
+    <ul class="  text-white">
+        <span class="text-white float-left"><a href="/" class="block p-3">Have Notes</a></span>
+        <li class="inline float-right"><a href="/upload" class="block p-3">Upload</a></li>
+        <li class="inline float-right"><a href="/classes" class="block p-3">Classes</a></li>
+        <li class="inline float-right"><a href="/" class="block p-3">Home</a></li>
     </ul>
 </nav> 
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    // console.log("Its working")
+</script>
+
+<span class="text-slate-800 text-3xl">
+    One stop solution for all your notes problem
+</span>
+
+<div class="grid">
+    <span>Grade 8</span>
+
+</div>
